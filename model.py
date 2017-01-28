e74d3e67120ffefaf373846680a43b3c9b4c4127
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2017-01-27 20:59:20 -0600

    Update model.py
    The difficulty of this competition is the kaggle python interrupter is really picky

--- model.py
+++ model.py
@@ -1,15 +1,22 @@
-#!/usr/bin/python3
+import kagglegym
+import numpy as np
 import pandas as pd
 
 
+
 class stockMarket:
-	def __init__(self):
-		self.loadData()
-
-	def loadData(self):
-		with pd.HDFStore("../input/train.h5", "r") as allData:
-			self.train = allData.get("train")
-		print(self.train.head())
-
+    def __init__(self):
+        '''
+            Initialize the dataset
+        '''
+        self.loadData()
+        
+    def loadData(self):
+        '''
+            Loads the data from an hdf5 file
+        '''
+        with pd.HDFStore("../input/train.h5", "r") as train:
+        # Note that the "train" dataframe is the only dataframe in the file
+            self.train = train.get("train")
 if __name__ == '__main__':
-	stockObj = stockMarket()	
+	stockObj = stockMarket()
