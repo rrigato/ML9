ab7e240d8725cec561835554fe71e45e511584d5
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2017-02-02 19:24:39 -0600

    Update model.py
    Trying to get the Kaggle Gym API to work

--- model.py
+++ model.py
@@ -29,7 +29,28 @@ class stockMarket:
         self.observation = self.env.reset()
         
         print(self.observation.train.head())
-        print(self.observation.train.shape)
+        print(self.observation.train.shape) 
+    
+    def viewTest(self):
+        '''
+            Viewing the test dataframe for Kaggle
+        '''
+        print(self.observation.target.head())
+        print(self.observation.target.shape)
+        #print(self.env.step(self.observation.target))
+        
+        
+        '''
+            looping through all observations
+        '''
+        while True:
+            observation, reward, done, info = self.env.step(self.observation.target)
+            if done:
+                break;
+        print(info)
+            
+            
 if __name__ == '__main__':
 	stockObj = stockMarket()
 	stockObj.createEnvironment()
+	stockObj.viewTest()
